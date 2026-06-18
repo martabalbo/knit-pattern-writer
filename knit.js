575339edd9289d36a4c4bee766310ac99555db56
Marta Balbo <marta99.balbo@gmail.com>
2026-06-18 12:06:52 +0200

    Update createGrid
    Change the createGrid function to built the grid by row instead of by column

--- knit.js
+++ knit.js
@@ -1,5 +1,3 @@
-//to do: fix toggle color function (enters the if, but no color)
-
 let grid = document.querySelector(".grid");
 
 function toggleColor() {
@@ -17,19 +15,16 @@ function toggleColor() {
     });
 };
 
-function createGrid() {
+function createGridByColumn() {
 
     // remove previous grid, if any
-
     removeGrid();
 
     // get user choice for the grid dimensions
-
     const gridWidth = document.getElementById("gridWidth").value;
     const gridHeight = document.getElementById("gridHeight").value;
 
     // create grid with the chosen dimensions
-
     for(let i=0; i<gridWidth; i++) {
         const column = document.createElement("div");
         column.className = "column";
@@ -45,7 +40,34 @@ function createGrid() {
     //grid.style.border = "1px solid black";
 
     // add toggle function to color the squares
+    toggleColor();
+};
 
+function createGridByRow() {
+
+    // remove previous grid, if any
+    removeGrid();
+
+    // get user choice for the grid dimensions
+    const gridWidth = document.getElementById("gridWidth").value;
+    const gridHeight = document.getElementById("gridHeight").value;
+
+    // create grid with the chosen dimensions
+    for(let i=0; i<gridHeight; i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+        for(let j=0; j<gridWidth; j++) {
+            const square = document.createElement("div");
+            square.className = "square";
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
+    };
+
+    // add border
+    //grid.style.border = "1px solid black";
+
+    // add toggle function to color the squares
     toggleColor();
 };
 
@@ -65,3 +87,7 @@ function clearGrid() {
         square.style.backgroundColor = "white";
     })
 };
+
+function generatePattern() {
+
+}
