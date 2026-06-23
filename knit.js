ba99c8974ef39b93b201adb897abfd1221f24f51
Marta Balbo <marta99.balbo@gmail.com>
2026-06-23 22:47:40 +0200

    Update knit.js
    Add function to generate a pattern with various colors. removeElement still does not work

--- knit.js
+++ knit.js
@@ -2,9 +2,8 @@
 
 let grid = document.querySelector(".grid");
 
-function toggleColor() {
+function toggleColor(chosenColor) {
     const squares = document.querySelectorAll(".square");
-    const chosenColor = document.getElementById("colors").value;
 
     Array.from(squares).forEach((square) => {
         square.addEventListener('click', (event) => {
@@ -17,10 +16,14 @@ function toggleColor() {
     });
 };
 
+function colorIn(chosenColor) {
+    toggleColor(chosenColor.value)
+}
+
 function createGridByColumn() {
 
     // remove previous grid, if any
-    removeGrid();
+    removeElement(grid);
 
     // get user choice for the grid dimensions
     const gridWidth = document.getElementById("gridWidth").value;
@@ -48,7 +51,7 @@ function createGridByColumn() {
 function createGridByRow() {
 
     // remove previous grid, if any
-    removeGrid();
+    removeElement(grid);
 
     // get user choice for the grid dimensions
     const gridWidth = document.getElementById("gridWidth").value;
@@ -73,11 +76,16 @@ function createGridByRow() {
     toggleColor();
 };
 
-function removeGrid() {
-    if (grid.hasChildNodes()) {
-        const rows = document.querySelectorAll(".row");
+function removeElement(element) {
+    if (element.hasChildNodes()) {
+        if (element == grid) {
+            const children = document.querySelectorAll(".row");
+        } else {
+            console
+            const children = document.querySelectorAll(".pattern-row");
+        }
 
-        Array.from(rows).forEach((child) => {
+        Array.from(children).forEach((child) => {
             child.parentNode.removeChild(child);
         });
     };
@@ -92,9 +100,15 @@ function clearGrid() {
 
 function generatePattern() {
     const rows = document.querySelectorAll(".row");
-    const chosenColor = document.getElementById("colors").value;
     const chosenNeedles = document.getElementById("needles").value;
     const patternBox = document.querySelector(".pattern");
+
+    const rowBox = document.createElement("div");
+    patternBox.appendChild(rowBox);
+    rowBox.style.fontWeight = 800;
+    rowBox.style.fontSize = "20px";
+    rowBox.style.marginBottom = "5px";
+    rowBox.textContent = "Pattern";
 
     let rowNumber = 0;
 
@@ -107,15 +121,32 @@ function generatePattern() {
         rowNumber += 1;
 
         Array.from(squares).forEach((square) => {
-            if (square.style.backgroundColor == chosenColor) {
-                currentRow.push("color");
-            } else {
-                currentRow.push("white");
+            switch(square.style.backgroundColor) {
+                case "rgb(0, 15, 137)":
+                    currentRow.push("blue")
+                    break
+                case "rgb(228, 0, 124)":
+                    currentRow.push("pink")
+                    break
+                case "rgb(139, 0, 139)":
+                    currentRow.push("purple")
+                    break
+                case "rgb(248, 186, 1)":
+                    currentRow.push("yellow")
+                    break
+                case "rgb(124, 220, 35)":
+                    currentRow.push("green")
+                    break
+                default:
+                    currentRow.push("white")
             }
         })
         if (rowNumber % 2 == 0 && chosenNeedles == "straight") {
             currentRow.reverse();
         }
+
+        // remove previous pattern, if any
+        removeElement(patternBox);
 
         // generate written pattern
         let mergedRow = []
@@ -124,17 +155,19 @@ function generatePattern() {
         
         mergedRow.forEach((element) => {
             //console.log(element)
-            if(element == "white" || element == "color") {
+            if(isNaN(element)) {
                 pattern += element + ", "
             } else {
                 pattern += element + " "
             }
         })
         const rowBox = document.createElement("div");
+        rowBox.className = ".pattern-row";
         patternBox.appendChild(rowBox);
         rowBox.textContent = pattern.slice(0, -2);
-
     })
+    //add border
+    pattern.style.border = "1px solid black";
 }
 
 // adds all adjacent and equal array elements, returns an array with quantites and colors in order
@@ -170,3 +203,5 @@ function printPattern() {
 // add option to use more than one color (up to 5/6?)
 // choose each color (basic colors), name it and draw in the grid
 // generate a pattern with the user's color names
+
+//removeElement fa casino
